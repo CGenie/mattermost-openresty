d99c0f988dfb0470930cb1de225c44add61e6ec2
Przemek Kaminski <cgenie@gmail.com>
2017-08-26 07:03:03 +0100

    mattermost/bitbucket: event-based template rendering
    We can use bitbucket's X-Event-Key header to check event type.

--- src/mattermost/bitbucket.lua
+++ src/mattermost/bitbucket.lua
@@ -7,80 +7,75 @@ local tools = require "tools"
 local mattermost_url = tools.get_env_variable_with_arg('BITBUCKET_MATTERMOST_URL', 'room', nil)
 local mattermost_user = tools.get_env_variable_with_arg('BITBUCKET_MATTERMOST_USER', 'user', 'bitbucket')
 
+local even = ngx.header["X-Event-Key"]
 local data_ = tools.get_ngx_data()
 local data = cjson.decode(data_)
 local message = nil
 local actor_tmpl = template.new("{% if actor.links.avatar.href then %}![embedded image]({* actor.links.avatar.href *}) {% end %}")
 local actor_display_tmpl = template.new("[{* actor.display_name *}]({* actor.links.html.href *})")
-local push_tmpl = template.new([[
-{* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})**/*[{* new_commit.name *}]({* new_commit.links.html.href *})* :: New commit from {* actor_display_tmpl *}:
-```
-{* new_commit.target.message *}
-```
-]])
-push_tmpl.actor_tmpl = actor_tmpl
-push_tmpl.actor_display_tmpl = actor_display_tmpl
-local pullrequest_tmpl = template.new([[
-{* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})** :: New pull request from {* actor_display_tmpl *}: *[{* pullrequest.title *}]({* pullrequest.links.html.href *})* ]])
-pullrequest_tmpl.actor_tmpl = actor_tmpl
-pullrequest_tmpl.actor_display_tmpl = actor_display_tmpl
-local commit_comment_tmpl = template.new([[
-{* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})**/*[{* comment.commit.hash *}]({* comment.commit.links.html.href *})* :: [New comment]({* comment.links.html.href *}) from {* user_display_tmpl *}:
-```
-{* comment.content.raw *}
-```
-]])
-commit_comment_tmpl.actor_tmpl = actor_tmpl
-commit_comment_tmpl.user_display_tmpl = actor_display_tmpl
-local pullrequest_comment_tmpl = template.new([[
-'{* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})**/*[PR {* comment.pullrequest.title *}]({* comment.pullrequest.links.html.href *})* :: [New comment]({* comment.links.html.href *}) from {* user_display_tmpl *}:
-```
-{* comment.content.raw *}
-```
-]])
-pullrequest_comment_tmpl.actor_tmpl = actor_tmpl
-pullrequest_comment_tmpl.user_display_tmpl = actor_display_tmpl
 
 if data.actor then
     actor_tmpl.actor = data.actor
 end
 
-local push = data.push
-if push then
+if event == 'repo:push' then
+    local push_tmpl = template.new([[
+        {* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})**/*[{* new_commit.name *}]({* new_commit.links.html.href *})* :: New commit from {* actor_display_tmpl *}:
+        ```
+        {* new_commit.target.message *}
+        ```
+        ]])
     actor_display_tmpl.actor = data.actor
+    push_tmpl.actor_tmpl = actor_tmpl
+    push_tmpl.actor_display_tmpl = actor_display_tmpl
     push_tmpl.repo = data.repository
-    push_tmpl.new_commit = push.changes[1].new
+    push_tmpl.new_commit = data.push.changes[1].new
     message = tostring(push_tmpl)
 end
 
-local pullrequest = data.pullrequest
-if pullrequest then
+if event == 'pullrequest:created' then
+    local pullrequest_tmpl = template.new([[
+        {* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})** :: New pull request from {* actor_display_tmpl *}: *[{* pullrequest.title *}]({* pullrequest.links.html.href *})* ]])
     actor_display_tmpl.actor = data.actor
-    pullrequest_tmpl.pullrequest = pullrequest
-    pullrequest_tmpl.repo = pullrequest.destination.repository
+    pullrequest_tmpl.actor_tmpl = actor_tmpl
+    pullrequest_tmpl.actor_display_tmpl = actor_display_tmpl
+    pullrequest_tmpl.pullrequest = data.pullrequest
+    pullrequest_tmpl.repo = data.pullrequest.destination.repository
     message = tostring(pullrequest_tmpl)
 end
 
-local comment = data.comment
-if comment then
-    actor_display_tmpl.actor = comment.user
+if event == 'repo:commit_comment_created' then
+    local commit_comment_tmpl = template.new([[
+        {* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})**/*[{* comment.commit.hash *}]({* comment.commit.links.html.href *})* :: [New comment]({* comment.links.html.href *}) from {* user_display_tmpl *}:
+        ```
+        {* comment.content.raw *}
+        ```
+        ]])
+    actor_display_tmpl.actor = data.comment.user
+    commit_comment_tmpl.actor_tmpl = actor_tmpl
+    commit_comment_tmpl.user_display_tmpl = actor_display_tmpl
+    commit_comment_tmpl.comment = data.comment
+    commit_comment_tmpl.repo = data.repository
+    message = tostring(commit_comment_tmpl)
+end
 
-    -- now question is whether the comment is for pull request or for commit
-    if comment.commit then
-        commit_comment_tmpl.comment = comment
-        commit_comment_tmpl.repo = data.repository
-        message = tostring(commit_comment_tmpl)
-    end
-
-    if comment.pullrequest then
-        pullrequest_comment_tmpl.comment = comment
-        pullrequest_comment_tmpl.repo = data.repository
-        message = tostring(pullrequest_comment_tmpl)
-    end
+if event == 'pullrequest:comment_created' then
+    local pullrequest_comment_tmpl = template.new([[
+        '{* actor_tmpl *} **[{* repo.full_name *}]({* repo.links.html.href *})**/*[PR {* comment.pullrequest.title *}]({* comment.pullrequest.links.html.href *})* :: [New comment]({* comment.links.html.href *}) from {* user_display_tmpl *}:
+        ```
+        {* comment.content.raw *}
+        ```
+        ]])
+    actor_display_tmpl.actor = data.comment.user
+    pullrequest_comment_tmpl.actor_tmpl = actor_tmpl
+    pullrequest_comment_tmpl.user_display_tmpl = actor_display_tmpl
+    pullrequest_comment_tmpl.comment = data.comment
+    pullrequest_comment_tmpl.repo = data.repository
+    message = tostring(pullrequest_comment_tmpl)
 end
 
 if not message then
-    message = 'This is not implemented yet: ' .. data_
+    message = 'This is not implemented yet (event ' .. event .. '): ' .. data_
 end
 
 ngx.say('message: ', message)
