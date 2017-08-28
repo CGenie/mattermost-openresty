1f5b34f0e5637e9f4ba1055b4b8785a2ac6794cd
Przemek Kaminski <cgenie@gmail.com>
2017-08-28 06:06:56 +0100

    Fix access by lua with auth token

--- src/auth.lua
+++ src/auth.lua
@@ -1,9 +1,9 @@
 tools = require("tools")
 
 local auth_token = os.getenv("AUTH_TOKEN")
-local query_token = ngx.var["arg_auth"]
+local query_token = ngx.var.arg_auth
 
-if auth_token ~= query_token then
+if (auth_token ~= query_token) then
     ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
